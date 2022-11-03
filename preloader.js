a25832630d5f24fe1073268e0c605a6447b8f461
anandkarthik39 <anandkarthik255@gmail.com>
2022-11-03 15:36:31 +0530

    created

---
+++ preloader.js
@@ -0,0 +1,12 @@
+
+
+  window.onload = 
+  function(){ 
+      setTimeout(
+    function(){ 
+        document.querySelector(".preloader").style.opacity = "0";
+
+    document.querySelector(".preloader").style.zIndex = "-99999";
+    },1000);
+  }
+
